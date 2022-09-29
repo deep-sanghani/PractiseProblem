f1709f8461822dd8b337466eede4a6866cb47209
deep-sanghani <106722680+deep-sanghani@users.noreply.github.com>
2022-09-29 11:18:31 +0530

    Add files via upload
    1,2,3,4 solution

---
+++ JavaPractiseProblem/src/main/java/Problem4.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class Problem4 {
+    public static void main(String[] args) {
+//        Given an array of n distinct elements. Find the minimum number of swaps required to sort the
+//        array in strictly increasing order
+//        int[] arr = {10, 19, 6, 3, 5};
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter numbers, with 0 to end");
+        List<Integer> arr = new ArrayList<>();
+        while (true) {
+            int input = scanner.nextInt();
+            if (input == 0) {
+                break;
+            }
+            arr.add(input);
+        }
+        List<Integer> nums = new ArrayList<>();
+        nums.addAll(arr);
+        int n = arr.size();
+        int ans = 0;
+        Collections.sort(arr);
+        for (int i = 0; i < n/2; i++) {
+            if(arr.get(i)!=nums.get(i)){
+                ans++;
+            }
+            else{
+                System.out.println("same");
+            }
+        }
+        System.out.println(nums);
+        System.out.println(arr);
+        System.out.println(ans);
+    }
+}
